cef59fe26461fb12780a840dffcd0241c20653ae
anchunmao <79773229+liangym2014@users.noreply.github.com>
2022-07-04 11:20:53 -0700

    Update Graph class.
    Update constructor with getline function.
    Initialize unions array with indices of the nodes.
    Delete merge function.
    Modify the function name of find_union to find, as well as the function signature.
    Update function RandomizedContracted signature and body.

--- Min Cut/Mincut.cpp
+++ Min Cut/Mincut.cpp
@@ -23,92 +23,60 @@ cut that you ever find.)
 #include <fstream>
 #include <string>
 #include <iostream>
+#include <sstream>
 #include <vector>
 #include <random>
+#include <numeric>
 
 using namespace std;
-
 
 class Graph {
 private:
     vector<pair<int,int>> edges;
-    vector<int> unions;  //0: undefined, >= 0: defined
+    vector<int> unions;
     vector<int> random_edge_order;
     int n;  //number of nodes
     
 
 public:
-    Graph(string filename, int num_nodes): n(num_nodes){
+    Graph(string filename, int num_nodes): n(num_nodes), unions(num_nodes + 1) {
         ifstream file(filename);
         if (file.is_open()) {
             string line;
             while (getline(file, line)) {
                 //the first one is vertex
-                int start = 0;
-                size_t pos = line.find('\t', start);
-                if (pos == string::npos)
-                    continue;
+                int i = 0, start;
+                string end;
+                istringstream _line = istringstream(line);
+                while (getline(_line, end, '\t')) {
+                    if ((i++) == 0) {
+                        start = stoi(end);
+                        continue;
+                    }
+                    int node = stoi(end);
+                    if (start < node)
+                        edges.emplace_back(start, node);
 
-                int node1 = stoi(line.substr(start, pos - start));
-                start = pos + 1;
-
-                pos = line.find('\t', start);
-                while (pos != string::npos) {
-                    int node2 = stoi(line.substr(start, pos - start));
-                    if(node1 < node2)
-                        edges.push_back({node1, node2}); //node1 < node2, remove self-loop and duplicate edges
-                    start = pos + 1;
-                    pos = line.find('\t', start);
                 }
-
             }
             file.close();
+            return;
         }
-        else
-            cout << "can't open file.\n";
+
+        cout << "can't open file.\n";
     }
 
-    int find_union(int idx) {
-        if (!idx)
-            return idx;
-
+    int find(vector<int>& unions, int idx) {
         if (unions[idx] != idx)
-            unions[idx] = find_union(unions[idx]);
+            unions[idx] = find(unions, unions[idx]);
 
         return unions[idx];
     }
 
-    void merge(int &node1, int &node2) {
-        if (!unions[node1]) {
-            if (!unions[node2]) {
-                unions[node1] = node1;
-                unions[node2] = node1;
-            }
-            else {
-                int node2_union_idx = find_union(unions[node2]);
-                unions[node1] = node2_union_idx;
-            }
-        }
-        else {
-            if (!unions[node2]) {
-                int node1_union_idx = find_union(unions[node1]);
-                unions[node2] = node1_union_idx;
-            }
-            else {
-                int node1_union_idx = find_union(unions[node1]);
-                int node2_union_idx = find_union(unions[node2]);
-
-                if (node1_union_idx != node2_union_idx)
-                    unions[node2_union_idx] = node1_union_idx;
-            }
-        }
-    }
-
-    int RandomizedContracted() {
+    int RandomizedContracted(vector<int> unions) {
         int remain = n;  //the number of remaining nodes
-        unions = vector<int>(n + 1);
         
-        //rearrange random_edge_order
+        //shuffle edges
         random_shuffle(random_edge_order.begin(), random_edge_order.end());
 
         int edge = 0;
@@ -117,11 +85,10 @@ public:
             pair<int, int> rand_edge = edges[random_edge_order[edge ++]];
 
             int node1 = rand_edge.first, node2 = rand_edge.second;
-            int node1_union_idx = find_union(unions[node1]);
-            int node2_union_idx = find_union(unions[node2]);
-            if (!node1_union_idx || !node2_union_idx || node1_union_idx != node2_union_idx){
+            int node1_union_idx = find(unions,node1), node2_union_idx = find(unions,node2);
+            if (node1_union_idx != node2_union_idx){
                 remain --;
-                merge(node1, node2);
+                unions[node2_union_idx] = node1_union_idx;
             }
         }
 
@@ -131,14 +98,9 @@ public:
             pair<int, int> rand_edge = edges[random_edge_order[edge ++]];
 
             int node1 = rand_edge.first, node2 = rand_edge.second;
-            int node1_union_idx = find_union(unions[node1]);
-            int node2_union_idx = find_union(unions[node2]);
+            int node1_union_idx = find(unions,node1), node2_union_idx = find(unions,node2);
 
-            if (!node1_union_idx)
-                unions[node1] = node1;
-            else if(!node2_union_idx)
-                unions[node2] = node2;
-            else if (node1_union_idx != node2_union_idx)
+            if (node1_union_idx != node2_union_idx)
                 count ++;
 
         }
@@ -148,16 +110,15 @@ public:
 
     int RepeatRandomizedContracted() {
         int i = n * n * log(n);
+        random_edge_order.resize(edges.size());
+        iota(random_edge_order.begin(), random_edge_order.end(), 0);  // indices for edges
+        iota(unions.begin(), unions.end(), 0);   // initial unions each vertex belongs to
 
-        //indices from 0 to edges.size() - 1
-        for (int j = 0; j < edges.size(); ++j)
-            random_edge_order.push_back(j);
+        cout << "start computing:\n";
+        int minCut = RandomizedContracted(unions);
 
-        int minCut = RandomizedContracted();
-        //cout << i << endl;
         while (-- i) {
-            int cut = RandomizedContracted();
-            //cout<<cut<<endl;
+            int cut = RandomizedContracted(unions);
             if (cut < minCut) 
                 minCut = cut;
         }
@@ -169,8 +130,8 @@ public:
 
 int main()
 {
-    vector<string> file{ "kargerMinCut.txt", "test.txt"};
-    vector<int> num_nodes = { 200, 8 };
+    string file[] = {"kargerMinCut.txt", "test.txt"};
+    int num_nodes[] = {200, 8};
     int i = 0;
 
     Graph graph(file[i], num_nodes[i]);
